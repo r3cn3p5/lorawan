9813228a7af8c2ad1d0064699a33853629a909d1
r3cn3p5 <59029127+r3cn3p5@users.noreply.github.com>
2021-11-06 23:11:04 +0000

    Create lora_states.py

---
+++ lora_states.py
@@ -0,0 +1,10 @@
+from micropython import const
+
+NOT_JOINED = const(0)
+JOINING = const(1)
+JOINED = const(2)
+CANNOT_JOIN = const(3)
+SENDING = const(4)
+RETRY = const(5)
+SENT = const(6)
+NOT_SENT = const(7)
